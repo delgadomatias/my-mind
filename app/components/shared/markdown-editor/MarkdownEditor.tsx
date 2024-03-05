32b68c07e6ce41aa440c9a8274778306b2c38402
Matias <mmatidelga2@gmail.com>
2024-03-05 19:50:27 -0300

    feat: add support for add image in post

--- app/components/shared/markdown-editor/MarkdownEditor.tsx
+++ app/components/shared/markdown-editor/MarkdownEditor.tsx
@@ -6,6 +6,7 @@ import Link from "@tiptap/extension-link";
 import Placeholder from "@tiptap/extension-placeholder";
 import { EditorContent, Extension, Extensions, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { Plugin } from "prosemirror-state";
 import styles from "./markdown-editor.module.css";
 
 export interface MarkdownEditorProps {
@@ -52,10 +53,104 @@ const MarkdownEditor = ({
     },
   });
 
+  const AddImageToNoteExtension = Extension.create({
+    addProseMirrorPlugins() {
+      return [
+        new Plugin({
+          props: {
+            handleDOMEvents: {
+              drop(view, event) {
+                const hasFiles =
+                  event.dataTransfer &&
+                  event.dataTransfer.files &&
+                  event.dataTransfer.files.length;
+
+                if (!hasFiles) {
+                  return;
+                }
+
+                const images = Array.from(event.dataTransfer.files).filter(
+                  (file) => /image/i.test(file.type)
+                );
+
+                if (images.length === 0) {
+                  return;
+                }
+
+                event.preventDefault();
+
+                const { schema } = view.state;
+                const coordinates = view.posAtCoords({
+                  left: event.clientX,
+                  top: event.clientY,
+                });
+
+                images.forEach((image) => {
+                  const reader = new FileReader();
+
+                  reader.onload = (readerEvent) => {
+                    const node = schema.nodes.image.create({
+                      src: readerEvent.target?.result,
+                    });
+                    const transaction = view.state.tr.insert(
+                      coordinates.pos,
+                      node
+                    );
+                    view.dispatch(transaction);
+                  };
+                  reader.readAsDataURL(image);
+                });
+              },
+              paste(view, event) {
+                const hasFiles =
+                  event.clipboardData &&
+                  event.clipboardData.files &&
+                  event.clipboardData.files.length;
+
+                if (!hasFiles) {
+                  return;
+                }
+
+                const images = Array.from(event.clipboardData.files).filter(
+                  (file) => /image/i.test(file.type)
+                );
+
+                if (images.length === 0) {
+                  return;
+                }
+
+                event.preventDefault();
+
+                const { schema } = view.state;
+
+                images.forEach((image) => {
+                  const reader = new FileReader();
+
+                  reader.onload = (readerEvent) => {
+                    const node = schema.nodes.image.create({
+                      src: readerEvent.target?.result,
+                    });
+                    const transaction =
+                      view.state.tr.replaceSelectionWith(node);
+                    view.dispatch(transaction);
+                  };
+                  reader.readAsDataURL(image);
+                });
+              },
+            },
+          },
+        }),
+      ];
+    },
+  });
+
   let extensions: Extensions = [
     StarterKit,
     Placeholder.configure({ placeholder: "Start typing here..." }),
-    Image,
+    Image.configure({
+      allowBase64: true,
+    }),
+    AddImageToNoteExtension,
     Link.configure({
       HTMLAttributes: {
         target: "_blank",
