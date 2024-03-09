65ac2922d832008939c5ea79e930ec225cb960fb
Matias <mmatidelga2@gmail.com>
2024-03-09 13:15:34 -0300

    feat: add .env

--- app/actions/upload.action.ts
+++ app/actions/upload.action.ts
@@ -1,21 +1,27 @@
 "use server";
 
-import { CloudinaryResponse } from "../interfaces/cloudinary-response.interface";
+import { CloudinaryResponse } from "@/interfaces/cloudinary-response.interface";
 
 export async function UploadImageAction(
-  form: FormData
+  form: FormData,
 ): Promise<CloudinaryResponse> {
-  form.append("upload_preset", "abgtsn7m");
-  form.append("api_key", "697916892814597");
+  if (
+    !process.env.CLOUDINARY_UPLOAD_PRESET ||
+    !process.env.CLOUDINARY_API_KEY
+  ) {
+    throw new Error("Missing UPLOAD_PRESET and API_KEY for Cloudinary");
+  }
+
+  form.append("upload_preset", process.env.CLOUDINARY_UPLOAD_PRESET!);
+  form.append("api_key", process.env.CLOUDINARY_API_KEY!);
 
   const response = await fetch(
     "https://api.cloudinary.com/v1_1/md-dlg/image/upload",
     {
       method: "POST",
       body: form,
-    }
+    },
   );
 
-  const data = (await response.json()) as CloudinaryResponse;
-  return data;
+  return (await response.json()) as CloudinaryResponse;
 }
