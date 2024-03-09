d7a9695633634460c2560b3b6fcb1b0c4616e844
Matias <mmatidelga2@gmail.com>
2024-03-09 19:25:15 -0300

    update: aspect of note item

--- app/components/features/notes/NoteList.tsx
+++ app/components/features/notes/NoteList.tsx
@@ -2,7 +2,7 @@
 
 import { getSortedNotes } from "@/app/utils/getSortedNotes";
 import { useNoteContext } from "@/context/notes";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { NoteItem } from "./NoteItem";
 
 export const NoteList = () => {
@@ -10,15 +10,17 @@ export const NoteList = () => {
   const sortedNotes = getSortedNotes(notes);
 
   return (
-    <motion.div
-      animate={{ opacity: 1, y: 0 }}
-      className="columns-1 sm:columns-2 md:columns-3 xl:columns-4 2xl:columns-5 gap-y-4"
-      initial={{ opacity: 0, y: 5 }}
-      transition={{ duration: 0.5, delay: 0.3 }}
-    >
-      {sortedNotes.map((note) => {
-        return <NoteItem note={note} key={`${note.id}+${note.content}`} />;
-      })}
-    </motion.div>
+    <AnimatePresence>
+      <motion.div
+        animate={{ opacity: 1, y: 0 }}
+        className="columns-1 sm:columns-2 md:columns-3 xl:columns-4 2xl:columns-6 gap-y-2"
+        initial={{ opacity: 0, y: 5 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+      >
+        {sortedNotes.map((note) => {
+          return <NoteItem note={note} key={`${note.id}+${note.content}`} />;
+        })}
+      </motion.div>
+    </AnimatePresence>
   );
 };
