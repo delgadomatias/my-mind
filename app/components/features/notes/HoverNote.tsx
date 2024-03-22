78542237eec92cc3b20c7a1afaa25c3c03ce08c1
Matias <mmatidelga2@gmail.com>
2024-03-22 15:14:40 -0300

    minor feat: add HoverNote component

---
+++ app/components/features/notes/HoverNote.tsx
@@ -0,0 +1,5 @@
+export const HoverNote = () => {
+  return (
+    <div className="border-4 border-[#B8C3D3] absolute inset-0 z-50 rounded-lg group-hover:opacity-100 opacity-0  transition-opacity duration-75 ease-in scale-[1.01] note-hover"></div>
+  );
+};
