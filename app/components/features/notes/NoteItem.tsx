79411d1c6faceb6bdbb1a86d2e449dde9c3242dc
Matias <mmatidelga2@gmail.com>
2024-03-22 15:15:25 -0300

    minor feat: add HoverNote component

--- app/components/features/notes/NoteItem.tsx
+++ app/components/features/notes/NoteItem.tsx
@@ -1,6 +1,7 @@
 import { isImageInContent } from "@/app/utils/isImageInContent";
 import { Note } from "@/interfaces/note.interface";
 import Link from "next/link";
+import { HoverNote } from "./HoverNote";
 import { NoteImage } from "./NoteImage";
 import { NoteText } from "./NoteText";
 
@@ -14,11 +15,11 @@ export const NoteItem = ({ note }: Props) => {
 
   return (
     <Link
-      className="hover:border-[#B8C3D3]  rounded-md flex hover:border-4 border-4 border-transparent transition-all duration-50 ease-linear h-fit break-inside-avoid z-10  max-w-full sm:max-w-[800px] relative"
+      className={`group border-4 border-transparent transition-all duration-50 ease-linear h-fit break-inside-avoid z-10  max-w-full sm:max-w-[800px] relative note-item`}
       href={id}
       key={id + content}
     >
-      <div className="border-4 border-[#B8C3D3] absolute inset-0 z-50 rounded-md group-hover:opacity-100 opacity-0  transition-opacity duration-75 ease-in scale-[1.02]"></div>
+      <HoverNote />
       {isImage && <NoteImage content={content} />}
       {!isImage && <NoteText content={content} />}
     </Link>
