9ee0e15084c16f90472b890e975a37e608af306f
Matias <mmatidelga2@gmail.com>
2024-03-28 18:32:33 -0300

    feat: add animation on hover in SearchNote

--- components/features/search-note/SearchNote.tsx
+++ components/features/search-note/SearchNote.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export const SearchNote = () => {
   const [searchValue, setsSearchValue] = useState("");
   const [hasTyped, setHasTyped] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
   const router = useRouter();
   const search = useSearchParams().get("search");
 
@@ -31,15 +33,23 @@ export const SearchNote = () => {
   }, [searchValue, router, hasTyped]);
 
   return (
-    <div className="mt-8">
+    <div className="relative mt-8">
       <input
         type="text"
         onChange={onChangeSearch}
         placeholder="Search my mind..."
         className="focus-none w-full bg-transparent font-louize text-7xl italic outline-none placeholder:opacity-70 hover:placeholder:opacity-50"
         defaultValue={search || ""}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       />
-      <hr className="mb-6 mt-4 bg-red-500" />
+      <hr className="mb-6 mt-4 border-black/10" />
+      <motion.div
+        className="absolute bottom-0 h-[0.05rem] w-full bg-black/40"
+        initial={{ width: 0 }}
+        animate={{ width: isHovered && !searchValue ? "100%" : 0 }}
+        transition={{ duration: 0.5 }}
+      ></motion.div>
     </div>
   );
 };
