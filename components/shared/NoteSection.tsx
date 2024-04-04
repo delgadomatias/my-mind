5927cdbdc585f84922f5cfea93c82a430159fc07
Matias <mmatidelga2@gmail.com>
2024-04-04 15:13:17 -0300

    minor-fix: change width on main container

--- components/shared/NoteSection.tsx
+++ components/shared/NoteSection.tsx
@@ -4,7 +4,7 @@ import { NoteList } from "../features/notes/NoteList";
 
 export const NoteSection = () => {
   return (
-    <div className="container mx-auto p-4 lg:w-[calc(100%_-_10rem)] lg:p-0">
+    <div className="container mx-auto max-w-[99.5%] p-4 lg:w-[calc(100%_-_10rem)] lg:p-0">
       <Suspense>
         <NoteContainer />
       </Suspense>
