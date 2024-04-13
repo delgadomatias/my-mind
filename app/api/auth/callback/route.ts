cea7d715833ada13237437a4e509f8cb59f181d5
Matias <mmatidelga2@gmail.com>
2024-04-12 22:19:32 -0300

    feat: add next cookie for callback route

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,11 +1,13 @@
 import { getDbOnRouteHandler } from "@/database/server";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
 
   const code = searchParams.get("code");
-  const next = searchParams.get("next") ?? "/";
+  const next = cookies().get("next")?.value || "/";
+  cookies().delete("next");
 
   if (code) {
     const supabase = await getDbOnRouteHandler();
