2e3073bf6b4818f57ac89e1d47d6de28a2df9c82
Matias <mmatidelga2@gmail.com>
2024-04-19 16:17:04 -0300

    feat: add toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Providers } from "@/Providers";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter, Nunito } from "next/font/google";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 const nunito = Nunito({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="en" className="h-full w-full bg-[#F0F2F5] light">
       <body className={`${nunito.className} min-h-[calc(100%_-_2rem)] w-full`}>
         <Providers>{children}</Providers>
+        <Toaster richColors />
       </body>
     </html>
   );
