b4a33032e2561efa33eda189a3eb4ea06cdfe9ef
Matias <mmatidelga2@gmail.com>
2024-04-19 16:32:04 -0300

    update: change confirm button color on alert

--- components/features/notes/note-details/modal/sidebar/CardActions.tsx
+++ components/features/notes/note-details/modal/sidebar/CardActions.tsx
@@ -31,7 +31,7 @@ export const CardActions = ({ onDeleteNote, note }: Props) => {
         text: "You won't be able to revert this!",
         icon: "warning",
         showCancelButton: true,
-        confirmButtonColor: "#3085d6",
+        confirmButtonColor: "#301934",
         cancelButtonColor: "#d33",
         confirmButtonText: "Yes, delete it!",
       },
