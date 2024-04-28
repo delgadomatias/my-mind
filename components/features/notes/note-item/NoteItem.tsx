7842f2b9b494e086880506caaf276b1d17fbb7d7
Matias <mmatidelga2@gmail.com>
2024-04-27 23:19:39 -0300

    update: NoteItem

--- components/features/notes/note-item/NoteItem.tsx
+++ components/features/notes/note-item/NoteItem.tsx
@@ -1,8 +1,19 @@
+"use client";
+
 import { Note } from "@/interfaces/note.interface";
 import { isImageInContent } from "@/utils/isImageInContent";
 import { HoverNote } from "./HoverNote";
 import { NoteImage } from "./NoteImage";
 import { NoteText } from "./NoteText";
+
+import {
+  deleteNote,
+  removeNoteFromTopOfMind,
+  setNoteOnTopOfMind,
+} from "@/actions/notes.action";
+import { Alerts } from "@/components/shared/adapters/alerts";
+import { TrashIcon } from "@/components/shared/icons/TrashIcon";
+import { Item, Menu, useContextMenu } from "react-contexify";
 
 interface Props {
   note: Note;
@@ -11,19 +22,56 @@ interface Props {
 export const NoteItem = ({ note }: Props) => {
   const { id, content } = note;
   const isImage = isImageInContent(content);
+  const menuId = `note-item-${id}`;
+  const { show } = useContextMenu({
+    id: menuId,
+  });
+
+  async function toggleTopOfMind(e: any) {
+    if (note.is_on_top) {
+      await removeNoteFromTopOfMind(id);
+      return;
+    }
+
+    await setNoteOnTopOfMind(id);
+  }
+
+  async function deleteCard(e: any) {
+    Alerts.askOrCancel({
+      options: {
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#301934",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      },
+      callback: async () => {
+        await deleteNote(id);
+      },
+    });
+  }
+
+  function displayMenu(e: any) {
+    show({
+      event: e,
+    });
+  }
 
   return (
-    <div className="flex flex-col gap-1 lg:gap-2">
+    <div className="flex flex-col gap-1 lg:gap-2" id="noteDiv">
       <div
         className={`duration-50 note-item group relative z-10 max-w-full cursor-pointer border-transparent transition-all ease-linear sm:max-w-[800px]`}
         draggable={false}
         onClick={() => {
           window.location.hash = id;
         }}
+        onContextMenu={displayMenu}
       >
         <HoverNote />
         {isImage && <NoteImage content={content} />}
-        {!isImage && <NoteText note={note} />}
+        {!isImage && <NoteText note={note} withFocusMode />}
       </div>
 
       {note.title && (
@@ -31,6 +79,29 @@ export const NoteItem = ({ note }: Props) => {
           {note.title}
         </p>
       )}
+
+      <Menu id={menuId}>
+        <Item onClick={toggleTopOfMind}>
+          <div className="inline-flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="22"
+              fill="#000000"
+              viewBox="0 0 256 256"
+            >
+              <path d="M248,124a56.11,56.11,0,0,0-32-50.61V72a48,48,0,0,0-88-26.49A48,48,0,0,0,40,72v1.39a56,56,0,0,0,0,101.2V176a48,48,0,0,0,88,26.49A48,48,0,0,0,216,176v-1.41A56.09,56.09,0,0,0,248,124ZM88,208a32,32,0,0,1-31.81-28.56A55.87,55.87,0,0,0,64,180h8a8,8,0,0,0,0-16H64A40,40,0,0,1,50.67,86.27,8,8,0,0,0,56,78.73V72a32,32,0,0,1,64,0v68.26A47.8,47.8,0,0,0,88,128a8,8,0,0,0,0,16,32,32,0,0,1,0,64Zm104-44h-8a8,8,0,0,0,0,16h8a55.87,55.87,0,0,0,7.81-.56A32,32,0,1,1,168,144a8,8,0,0,0,0-16,47.8,47.8,0,0,0-32,12.26V72a32,32,0,0,1,64,0v6.73a8,8,0,0,0,5.33,7.54A40,40,0,0,1,192,164Zm16-52a8,8,0,0,1-8,8h-4a36,36,0,0,1-36-36V80a8,8,0,0,1,16,0v4a20,20,0,0,0,20,20h4A8,8,0,0,1,208,112ZM60,120H56a8,8,0,0,1,0-16h4A20,20,0,0,0,80,84V80a8,8,0,0,1,16,0v4A36,36,0,0,1,60,120Z"></path>
+            </svg>
+            <p>{note.is_on_top ? "Remove from Top of Mind" : "Top of Mind"}</p>
+          </div>
+        </Item>
+        <Item onClick={deleteCard}>
+          <div className="inline-flex items-center gap-2 text-red-500">
+            <TrashIcon className="fill-red-500" width="22" height="22" />
+            <p>Delete card</p>
+          </div>
+        </Item>
+      </Menu>
     </div>
   );
 };
--- components/features/notes/note-item/NoteText.tsx
+++ components/features/notes/note-item/NoteText.tsx
@@ -9,9 +9,10 @@ import { useState } from "react";
 
 interface Props {
   note: Note;
+  withFocusMode?: boolean;
 }
 
-export const NoteText = ({ note }: Props) => {
+export const NoteText = ({ note, withFocusMode = false }: Props) => {
   const [isOnFocus, setIsOnFocus] = useState(false);
   const { content, id, title } = note;
   const { length } = content;
@@ -20,7 +21,7 @@ export const NoteText = ({ note }: Props) => {
 
   return (
     <div
-      className={`w-full  ${length > 500 ? "note-long" : ""} rounded-lg bg-white px-3 py-2 shadow-xl lg:px-6 lg:py-4`}
+      className={`h-full  ${length > 500 ? "note-long" : ""} rounded-lg bg-white px-3 py-2 shadow-xl lg:px-6 lg:py-4`}
       style={
         length > 500
           ? {
@@ -35,31 +36,33 @@ export const NoteText = ({ note }: Props) => {
         className={`w-full py-2 ${classesByTagToApply}`}
         key={id + content + title}
       />
-      <span
-        className="group absolute right-2 top-2 z-50 hidden flex-col  items-center gap-2 lg:flex"
-        onClick={() => {
-          router.push(`#${id}?focus=true`);
-        }}
-      >
-        <Image
-          src="https://static.accelerator.net/134/0.27.1/icons/focus-circle-light.png"
-          alt="Toggle Focus Mode"
-          height={14}
-          width={14}
-          className="!opacity-0 blur-[2px] transition-all duration-150 ease-linear hover:animate-spin hover:blur-0 group-hover:!opacity-100"
-          onMouseEnter={() => setIsOnFocus(true)}
-          onMouseLeave={() => setIsOnFocus(false)}
-        />
-
+      {withFocusMode && (
         <span
-          className="focus-tooltip !pointer-events-none absolute -top-8 rounded-md bg-[#F0F2F5] px-4 py-1 text-sm font-medium text-black opacity-0 shadow-xl transition-all duration-150 ease-linear"
-          style={{
-            opacity: isOnFocus ? 1 : 0,
+          className="group absolute right-2 top-2 z-50 hidden flex-col  items-center gap-2 lg:flex"
+          onClick={() => {
+            router.push(`#${id}?focus=true`);
           }}
         >
-          Focus
+          <Image
+            src="https://static.accelerator.net/134/0.27.1/icons/focus-circle-light.png"
+            alt="Toggle Focus Mode"
+            height={14}
+            width={14}
+            className="!opacity-0 blur-[2px] transition-all duration-150 ease-linear hover:animate-spin hover:blur-0 group-hover:!opacity-100"
+            onMouseEnter={() => setIsOnFocus(true)}
+            onMouseLeave={() => setIsOnFocus(false)}
+          />
+
+          <span
+            className="focus-tooltip !pointer-events-none absolute -top-8 rounded-md bg-[#F0F2F5] px-4 py-1 text-sm font-medium text-black opacity-0 shadow-xl transition-all duration-150 ease-linear"
+            style={{
+              opacity: isOnFocus ? 1 : 0,
+            }}
+          >
+            Focus
+          </span>
         </span>
-      </span>
+      )}
     </div>
   );
 };
