a0d70f589e661344ff4a93859a152b0c6fbc5fbc
Matias <mmatidelga2@gmail.com>
2024-04-27 23:17:17 -0300

    update: get user from public schema

--- utils/getUser.ts
+++ utils/getUser.ts
@@ -1,16 +1,30 @@
 import { getDbOnClient } from "@/database/client";
 import { getDbOnServerComponent } from "@/database/server";
+import { PublicUser } from "@/interfaces";
 
 export const getUser = async () => {
   if (typeof window === "undefined") {
     const supabase = await getDbOnServerComponent();
     const { data } = await supabase.auth.getSession();
     const { session } = data;
-    return session?.user;
+    const { data: user } = await supabase
+      .from("User")
+      .select("*")
+      .eq("email", session?.user.email)
+      .single();
+
+    return user as PublicUser;
   }
 
   const supabase = getDbOnClient();
-  const { data } = await supabase.auth.getUser();
-  const { user } = data;
-  return user;
+  const { data } = await supabase.auth.getSession();
+  const { session } = data;
+
+  const { data: user } = await supabase
+    .from("User")
+    .select("*")
+    .eq("email", session?.user.email)
+    .single();
+
+  return user as PublicUser;
 };
